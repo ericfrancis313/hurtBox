c7530d406699cd099d4d1f6a5247f506bdcb0fb3
Eric <EricFrancis313@gmail.com>
2019-06-18 23:15:28 -0400

    adding react

---
+++ app/javascript/react/components/CharacterSelect.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class CharacterSelect extends React.Component {
+  constructor(props){
+    super(props);
+    this.state ={
+
+    }
+  }
+
+  render(){
+    return(
+      <h1>Select your fighter</h1>
+    )
+  }
+}
+
+export default CharacterSelect
