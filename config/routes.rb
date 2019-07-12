03910d57aa5dccde65230f55a71b132e7e2c0a6e
Eric <EricFrancis313@gmail.com>
2019-07-11 22:39:17 -0400

    adding characters to game show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :games, only: [:index,:show]
+  resources :characters, only: [:index, :show]
 
   namespace :api do
     namespace :v1 do
-      resources :games, only:[:show]
-    end 
+      resources :games, only: [:show] do
+        resources :characters, only: [:show]
+      end
+    end
   end
 end
