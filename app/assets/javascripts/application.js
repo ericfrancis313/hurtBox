f1f942d3e90f0332103eec3f5cb3fdc7f91f39b5
Eric <EricFrancis313@gmail.com>
2021-05-27 12:21:10 -0400

    proc

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,3 @@
 
 //var PORT = process.env.Port
 //$(function(){ $(document).foundation(); });
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
