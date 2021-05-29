c6724c3806e9685f246f5a69245b29235f7eb3b0
Eric <EricFrancis313@gmail.com>
2021-05-29 10:37:31 -0400

    :/

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,8 +17,3 @@
 
 //var PORT = process.env.Port
 //$(function(){ $(document).foundation(); });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
